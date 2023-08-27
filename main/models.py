0dc7fb2952ae8e08dfc5bd6f267f0a8b10891dc6
pi_kazhgin <pi_kazhgin@controlsystems.ru>
2023-08-27 19:35:38 +0300

    Release v1.0.0

---
+++ main/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Task(models.Model):
+    title = models.CharField('Название', max_length=50)
+    task = models.TextField('Описание')
+
+    def __str__(self):
+        return self.title
+    class Meta:
+        verbose_name = 'Zadacha'
+        verbose_name_plural = 'Zadachi'
